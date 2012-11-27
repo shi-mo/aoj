14b2a493a36393a2a47ec377fcd7629a58961718
shi-mo <yoshifumi.shimono@gmail.com>
2012-11-27 12:20:27 +0900

    Refactoring a testset for problem 0500.

--- 0500/gen_1_1_atomic.rb
+++ 0500/gen_1_1_atomic.rb
@@ -1,23 +1,28 @@
-(0..9).each do |i|
-  (0..9).each do |j|
-    basename = "1_#{i}#{j}_atomic"
-    open("#{basename}.input", 'w') do |f|
+basename = "1_1_atomic"
+
+open("#{basename}.input", 'w') do |f|
+  (0..9).each do |i|
+    (0..9).each do |j|
       f.puts '1'
       f.puts "#{i} #{j}"
-      f.puts '0'
     end
-    open("#{basename}.output", 'w') do |f|
+  end
+  f.puts '0'
+end
+open("#{basename}.output", 'w') do |f|
+  (0..9).each do |i|
+    (0..9).each do |j|
       if i == j
         f.puts "#{i} #{j}"
-        break
+        next
       end
       if i < j
         f.puts "0 #{i+j}"
-        break
+        next
       end
       if j < i
         f.puts "#{i+j} 0"
-        break
+        next
       end
       raise 'must not happen'
     end

88ca09031fb335363fc2f3ebc429dd2c2ded7f81
shi-mo <yoshifumi.shimono@gmail.com>
2012-11-29 12:48:46 +0900

    Boundary test for problem 0112.

---
+++ 0112/gen_3_2.rb
@@ -0,0 +1,15 @@
+basename = '_3_2_value_bound_60_10000sets'
+
+n = 10000
+minutes = 60
+
+open("#{basename}.input", 'w') do |f|
+  f.puts n
+  n.times{ f.puts minutes }
+  f.puts '0'
+end
+
+open("#{basename}.output", 'w') do |f|
+  wait = minutes * n * (n-1) / 2
+  f.puts wait
+end

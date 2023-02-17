02c82377a81d41257edb20da94dd00126db5faca
shi-mo <yoshifumi.shimono@gmail.com>
2023-02-17 22:46:14 +0900

    Simplify ITP2_3_D.py

--- ITP2_3_D.py
+++ ITP2_3_D.py
@@ -1,12 +1,8 @@
 import sys
 
-n = int(sys.stdin.readline())
+_ = int(sys.stdin.readline())
 a = [int(w) for w in sys.stdin.readline().split()]
-m = int(sys.stdin.readline())
+_ = int(sys.stdin.readline())
 b = [int(w) for w in sys.stdin.readline().split()]
 
-for i in range(0,min(n,m)):
-    if a[i] == b[i]: continue
-    print(1 if a[i] < b[i] else 0)
-    sys.exit(0)
-print(1 if n < m else 0)
+print(1 if a < b else 0)

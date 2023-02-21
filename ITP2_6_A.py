8d69120ecff068807e1f94345f8935b1b383630d
shi-mo <yoshifumi.shimono@gmail.com>
2023-02-21 22:21:39 +0900

    ITP2_6_A

---
+++ ITP2_6_A.py
@@ -0,0 +1,11 @@
+from bisect import bisect_left
+
+n = int(input())
+a = [int(x) for x in input().split()]
+q = int(input())
+
+l = len(a)
+for i in range(0,q):
+    k = int(input())
+    i = bisect_left(a, k)
+    print(1 if i < l and a[i] == k else 0)

abb56bbf236032497053f938ac08b99cc4d8c2d2
shi-mo <yoshifumi.shimono@gmail.com>
2023-02-23 01:56:45 +0900

    Fasten ITP2_5_C

--- ITP2_5_C.py
+++ ITP2_5_C.py
@@ -4,33 +4,34 @@ a = [int(x) for x in input().split()]
 def next_of(a: list, desc: bool):
     p = a.copy()
     if desc: p = [-x for x in p]
+    n = len(a)
 
-    escape = False
-    for j in reversed(range(1,n)):
-        if escape: break
-        for i in reversed(range(0,j)):
-            v = max(p[i+1:])
-            if p[i] > v: continue
+    l = -1
+    for i in reversed(range(1,n)):
+        if p[i-1] > p[i]: continue
+        l = i-1
+        break
 
-            b = list(sorted(p[i:].copy()))
-            idx = b.index(p[i])
-            if (len(b)-1) <= idx: continue
+    if l < 0:
+        if desc: p = [-x for x in p]
+        return p
 
-            p[i] = b[idx+1]
-            b.remove(b[idx+1])
-            p[i+1:] = sorted(b)
-            escape = True
-            break
+    r = l+1
+    for j in range(l+2,n):
+        if p[l] > p[j]: continue
+        if p[r] < p[j]: continue
+        r = j
+
+    p[l],p[r] = p[r],p[l]
+    p[l+1:] = sorted(p[l+1:])
 
     if desc: p = [-x for x in p]
     return p
 
 p = next_of(a, desc=True)
-if p != a:
-    print(*p)
+if p != a: print(*p)
 
 print(*a)
 
 b = next_of(a, desc=False)
-if b != a:
-    print(*b)
+if b != a: print(*b)

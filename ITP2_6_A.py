3e4ce4594971bc86859dc327f119f06209f96ce7
shi-mo <yoshifumi.shimono@gmail.com>
2023-02-22 20:02:45 +0900

    Refactor ITP2_6_A

--- ITP2_6_A.py
+++ ITP2_6_A.py
@@ -5,7 +5,7 @@ a = [int(x) for x in input().split()]
 q = int(input())
 
 l = len(a)
-for i in range(0,q):
+for _ in range(0,q):
     k = int(input())
     i = bisect_left(a, k)
     print(1 if i < l and a[i] == k else 0)

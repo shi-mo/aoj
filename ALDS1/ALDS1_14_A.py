173247959627421fc28de0d586a3b2f82aa72dd4
shi-mo <yoshifumi.shimono@gmail.com>
2025-01-04 04:27:25 +0000

    Refactor ALDS1_14_A.py

--- ALDS1/ALDS1_14_A.py
+++ ALDS1/ALDS1_14_A.py
@@ -5,14 +5,8 @@ def main():
     p = input()
     pat = re.compile(re.escape(p))
 
-    for pos in find_all_matches_in(t, pat):
-        print(pos)
-
-def find_all_matches_in(s, pat):
-    matches = []
-    for i in range(len(s)):
-        m = re.search(pat, s[i:])
-        if m: matches.append(i+m.start())
-    return list(dict.fromkeys(matches))
+    for i in range(len(t)):
+        if t[i:].startswith(p):
+            print(i)
 
 main()
